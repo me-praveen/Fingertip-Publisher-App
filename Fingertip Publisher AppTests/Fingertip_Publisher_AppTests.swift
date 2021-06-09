f5466009af46d64437af23619187d8bed5f29f27
T, Praveen <praveen.t@clarivate.com>
2021-06-10 04:02:22 +0530

    Initial Commit

---
+++ Fingertip Publisher AppTests/Fingertip_Publisher_AppTests.swift
@@ -0,0 +1,33 @@
+//
+//  Fingertip_Publisher_AppTests.swift
+//  Fingertip Publisher AppTests
+//
+//  Created by T, Praveen on 10/06/21.
+//
+
+import XCTest
+@testable import Fingertip_Publisher_App
+
+class Fingertip_Publisher_AppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
